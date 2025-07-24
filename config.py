26ba80e12f2f0d4d004c20f435fc3ead103f6a39
Ethan Cai <ethan.cai1999@gmail.com>
2025-07-24 01:05:17 -0400

    Considerable update

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 MONGO_URI   = os.environ.get("MONGO_URI")
 SECRET_KEY  = os.environ.get("SECRET_KEY")
 
-# (Optional) Fail fast if they’re missing:
 if not MONGO_URI or not SECRET_KEY:
     raise RuntimeError("MONGO_URI and SECRET_KEY must be set in the environment")
